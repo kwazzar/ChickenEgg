509244b69ddc867a0499f24c44b16a62a9b21a2c
kwazzar <fugitiwe@gmail.com>
2025-12-07 22:45:35 +0200

    Initial commit

---
+++ ChickenEgg/ChickenEggApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenEggApp.swift
+//  ChickenEgg
+//
+//  Created by Quasar on 07.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChickenEggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
